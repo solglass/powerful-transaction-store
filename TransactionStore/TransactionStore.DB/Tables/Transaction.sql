6ac3fcca1b0344ac14d8b6ae25d001bcfd0508d1
Denis Valeev <valeevden@gmail.com>
2021-03-31 23:46:06 +0300

    Fix transaction table

--- TransactionStore/TransactionStore.DB/Tables/Transaction.sql
+++ TransactionStore/TransactionStore.DB/Tables/Transaction.sql
@@ -1,7 +1,16 @@
-﻿CREATE TABLE [dbo].[Transaction](
+﻿CREATE TABLE [dbo].[Transaction]
+(
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[LeadId] [int] NOT NULL,
 	[Amount] [int]  NOT NULL,
 	[Type] [int] NOT NULL,
-	[Timestamp] [datetime2](7) NOT NULL
-	)
+	[Timestamp] [datetime2](7) NOT NULL,
+
+	CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED ([Id] ASC) 
+	WITH 
+		( PAD_INDEX = OFF, 
+		STATISTICS_NORECOMPUTE = OFF, 
+		IGNORE_DUP_KEY = OFF, 
+		ALLOW_ROW_LOCKS = ON,
+		ALLOW_PAGE_LOCKS = ON)
+) 
