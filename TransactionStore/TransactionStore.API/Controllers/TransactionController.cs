0b58d04c26d36c94f5e93ad85c2ce6f6841bec1f
Denis Valeev <valeevden@gmail.com>
2021-04-02 05:54:13 +0300

    add "scelet" for Controller

---
+++ TransactionStore/TransactionStore.API/Controllers/TransactionController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TransactionStore.Business;
+
+namespace TransactionStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private ITransactionService _transactionService;
+        private IMapper _mapper;
+        
+        public TransactionController(IMapper mapper, ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+            _mapper = mapper;
+        }
+
+        
+
+
+    }
+}
