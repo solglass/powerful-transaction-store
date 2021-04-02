1757e6c4920fc8f9dca3567c08083a5c5c1bef7f
Denis Valeev <valeevden@gmail.com>
2021-04-02 05:07:10 +0300

    Add Transaction repository and interface

---
+++ TransactionStore/TransactionStore.Data/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TransactionStore.Data.Models;
+
+namespace TransactionStore.Data
+{
+    public interface ITransactionRepository
+    {
+        int AddDepositeOrWithdraw(TransactionDto dto);
+        int AddTransfer(TransferDto dto);
+        int DeleteTransaction(int transactionId);
+        TransactionDto GetTransactionById(int transactionId);
+        TransactionDto GetTransactionByLeadId(int transactionId);
+        TransferDto GetTransferBydId(int transactionId);
+        LeadBalanceDto GetBalanceByLeadId(int leadId, DateTime date);
+    }
+}
---
+++ TransactionStore/TransactionStore.Data/TransactionRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using TransactionStore.Core.Settings;
+using TransactionStore.Data.Models;
+
+namespace TransactionStore.Data
+{
+    public class TransactionRepository : BaseRepository, ITransactionRepository
+    {
+        public TransactionRepository(IOptions<AppSettings> options) : base(options)
+        {
+            _connection = new SqlConnection(_connectionString);
+        }
+
+        public int AddDepositeOrWithdraw(TransactionDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddTransfer(TransferDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteTransaction(int transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeadBalanceDto GetBalanceByLeadId(int leadId, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TransactionDto GetTransactionById(int transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TransactionDto GetTransactionByLeadId(int transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TransferDto GetTransferBydId(int transactionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
