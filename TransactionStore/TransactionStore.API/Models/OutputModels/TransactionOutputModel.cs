389ebc07b77843963b6490c9ca9002a87d15170c
solglass <solglass@yandex.ru>
2021-04-04 15:27:56 +0300

    LeadId made nullable

--- TransactionStore/TransactionStore.API/Models/OutputModels/TransactionOutputModel.cs
+++ TransactionStore/TransactionStore.API/Models/OutputModels/TransactionOutputModel.cs
@@ -8,7 +8,7 @@ namespace TransactionStore.API.Models.InputModels
     public class TransactionOutputModel
     {
         public int Id { get; set; }
-        public virtual int LeadId { get; set; }
+        public int? LeadId { get; set; }
         public decimal Amount { get; set; }
         public string Currency { get; set; }
         public string Type { get; set; }
