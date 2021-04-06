028ec75d76e96ec78e2454e16206acf2c7bc8f0a
SimonShvets <goodweather.spb@gmail.com>
2021-04-06 23:27:27 +0300

    method friendly names for currency is deleted

--- TransactionStore/TransactionStore.Core/Enums/FriendlyNames.cs
+++ TransactionStore/TransactionStore.Core/Enums/FriendlyNames.cs
@@ -6,20 +6,6 @@ namespace EducationSystem.Core.Enums
 {
     public static class FriendlyNames
     {
-        public static string GetFriendlyCurrencyName(Currency currencyType)
-        {
-
-            string friendlyName = currencyType switch
-            {
-                Currency.RUB => "Рубль",
-                Currency.USD => "Доллар",
-                Currency.EUR => "Евро",
-                Currency.JPY => "Иена",
-                _ => "Некорректная валюта"
-            };
-            return friendlyName;
-
-        }
 
         public static string GetFriendlyTransactionTypeName(TransactionType transactionType)
         {
