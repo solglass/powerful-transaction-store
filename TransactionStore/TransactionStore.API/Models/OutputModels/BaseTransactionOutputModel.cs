7d1eae704dcb252a07c3639314c6683f3a84e65c
SimonShvets <goodweather.spb@gmail.com>
2021-04-09 18:39:59 +0300

    id to outputmodel added

--- TransactionStore/TransactionStore.API/Models/OutputModels/BaseTransactionOutputModel.cs
+++ TransactionStore/TransactionStore.API/Models/OutputModels/BaseTransactionOutputModel.cs
@@ -3,6 +3,7 @@ namespace TransactionStore.API.Models.OutputModels
 {
     public abstract class BaseTransactionOutputModel
     {
+        public int Id { get; set; }
         public decimal Amount { get; set; }
         public string Currency { get; set; }
         public string Type { get; set; }
