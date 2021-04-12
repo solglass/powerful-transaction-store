23592646f5d05e2e65305440885d73788cfd0dcd
SimonShvets <goodweather.spb@gmail.com>
2021-04-12 19:03:06 +0300

    fix validation

--- TransactionStore/TransactionStore.API/Attributes/CustomCurrencyPairValidation.cs
+++ TransactionStore/TransactionStore.API/Attributes/CustomCurrencyPairValidation.cs
@@ -6,10 +6,11 @@ namespace TransactionStore.API.Attributes
 {
     public class CustomCurrencyPairValidation : ValidationAttribute
     {
+        private const int _currancyPairLength = 6;
         public override bool IsValid(object value)
         {
             string currancyPair = (string)value;
-            return currancyPair.Length == 6
+            return currancyPair.Length == _currancyPairLength
                 && Quotes.CurrencyPair.ContainsKey(currancyPair.Substring(0, 3) + Quotes.baseCurrency) 
                 && Quotes.CurrencyPair.ContainsKey(currancyPair.Substring(3, 3) + Quotes.baseCurrency);
         }
